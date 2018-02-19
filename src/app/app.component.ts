36afba0d5d0ad3ba0082edf49be040143330429a
cappors <cappors@gmail.com>
2018-02-19 09:12:30 +0100

    auth service aot fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit {
     date: any = new Date().getTime();
     setupTime: any = localStorage.getItem('created');
 
-    constructor(private sharedService: SharedService, private authService: AuthService, private router: Router) {
+    constructor(private sharedService: SharedService, public authService: AuthService, private router: Router) {
         //Check user login
         if ((this.date - this.setupTime) > this.localStorageMaxTime * 60 * 60 * 1000) {
             localStorage.clear();
